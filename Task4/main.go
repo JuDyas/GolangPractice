55df971155d7fd2b669303448aa027ad83c84939
JuDyas <gden886@gmail.com>
2024-09-30 15:03:43 +0200

    Calculator 1.2

--- Task4/main.go
+++ Task4/main.go
@@ -1,34 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	calculate()
 }
 
-func calculate() {
-	var num1, num2, answer float64
-	var operator string
+func calculate() { //
+	fmt.Println("Enter an expression, for example 2 + 2 * 2")
+	read := bufio.NewReader(os.Stdin)      //Запрашивает пример
+	readText, _ := read.ReadString('\n')   //Ожидание ввода до нажатия enter(/m)
+	readText = strings.TrimSpace(readText) // Должно очистить лишние пробелы если они будут (в начале и конце строки)
 
-	fmt.Println("Enter an expression, for example 2 + 2")
-	fmt.Scanf("%f %s %f", &num1, &operator, &num2) //Запрашивает пример
+	splitText := strings.Fields(readText) //Разбиваем строку на отдельные элементы (по пробелам)
+	splitText = multyply(splitText)       //Деление и умножение
+	answer := addsubst(splitText)         //Сочетание и вычетание
+	fmt.Println(answer)                   //Вывод ответа
+}
 
-	switch operator { //Выбираем оператор (+, -, * или /)
-	case "+":
-		answer = num1 + num2
-	case "-":
-		answer = num1 - num2
-	case "*":
-		answer = num1 * num2
-	case "/":
-		if num2 == 0 { //При попытке делить на 0 выводим ошибку
-			fmt.Println("Error: You can't divide by 0")
-		} else {
-			answer = num1 / num2
+func multyply(expression []string) []string { //Умножение с делением идут первыми, поэтому сделал отдельные функции чтобы пройтись сначала по ним.
+	var result float64 //Переменная сохраняет результат отдельных операций между 2мя числами.
+
+	i := 0
+	for i < len(expression) {
+		element := expression[i] //Текущий эллемент
+
+		if element == "*" || element == "/" {
+			if i-1 >= 0 && i+1 < len(expression) { //Проверка на существвование индекса, воизбежание выхода за массив
+				prev, _ := strconv.ParseFloat(expression[i-1], 64)
+				next, _ := strconv.ParseFloat(expression[i+1], 64) //Меняем тип данных для предыдущего и следующего элемента после оператора
+
+				if element == "*" {
+					result = prev * next
+				} else {
+					result = prev / next
+				}
+
+				expression[i-1] = fmt.Sprintf("%f", result)              // Заменяем предыдущий элемент на результат вычислеи
+				expression = append(expression[:i], expression[i+2:]...) // Удаляем оператор и следующий элемент
+				i--                                                      // Корректируем индекс чтобы можно было снова пройтись по оператору
+			}
 		}
-	default: //Если ввод не соотвествует стандарту выдаём ошибку.
-		fmt.Println("Error: Invalid input. Please use +, -, *, or /, and separate by spaces.")
-		return
+		i++
 	}
-	fmt.Println("Answer:", answer)
+	return expression //Возвращаем изменённый массив
+}
+func addsubst(expression []string) string {
+	var result float64
+	result, _ = strconv.ParseFloat(expression[0], 64) //Меняем тип первого эллемента
+
+	i := 1
+	for i < len(expression) {
+		element := expression[i]
+
+		if i+1 < len(expression) {
+			next, _ := strconv.ParseFloat(expression[i+1], 64)
+
+			if element == "+" {
+				result += next
+			} else {
+				result -= next
+			}
+		}
+		i += 2
+	}
+	return fmt.Sprintf("%f", result) //Вывод результата
 }
