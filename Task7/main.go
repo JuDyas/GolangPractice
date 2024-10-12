241f3712a275d06efa08a2a12e78b6752525f7bb
JuDyas <gden886@gmail.com>
2024-10-12 03:07:20 +0200

    Hashing 2.0.0

--- Task7/main.go
+++ Task7/main.go
@@ -1,84 +1,92 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"io"
-	"os"
+	"log"
+	"strings"
 )
 
 func main() {
-	fileNameIn := "./example.json"
-	fileNameOut := "exampleSHA1.json"
-	whatHash := []string{"password", "ip"}
+	jsonData := flag.String("json", `{"name": "Denys", "age": "123", "password": "UpbQ2X*&FQ$L", "info": {"weight": "60", "ip": "192.168.1.1"}}`, "Input JSON data")
+	hashType := flag.String("hash", "sha1", "Hash Type")
+	whatHash := flag.String("hashElement", "password,ip", "Elements to hash")
+	flag.Parse()
 
-	err := readWrite(fileNameIn, fileNameOut, whatHash)
+	dataProcessed, whatHashProcessed, err := processFlags(jsonData, whatHash)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		log.SetPrefix("ERROR: ")
+		log.Fatalf("Processing data: %v", err)
 	}
+	resultJson, err := hashJson(dataProcessed, whatHashProcessed, hashType)
+	if err != nil {
+		log.SetPrefix("ERROR: ")
+		log.Fatalf("During hashing: %v", err)
+	}
+	log.Println(resultJson)
 }
 
-// readWrite - open/read/write json file
-func readWrite(fileNameIn, fileNameOut string, whatHash []string) error {
-	file, err := os.Open(fileNameIn)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	readerByte, err := io.ReadAll(file)
-	if err != nil {
-		return err
-	}
-
+// processFlags - processing input data to map and slice
+func processFlags(jsonData, hashElements *string) (map[string]interface{}, []string, error) {
 	var data map[string]interface{}
-	err = json.Unmarshal(readerByte, &data)
+	err := json.Unmarshal([]byte(*jsonData), &data)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
-	hashJson(data, whatHash)
-
-	writeJson, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(fileNameOut, writeJson, 0644)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Complete")
-	return nil
+	whatHash := strings.Split(*hashElements, ",")
+	log.SetPrefix("INFO: ")
+	log.Println("Processing frags complete", whatHash, data)
+	return data, whatHash, nil
 }
 
-// sha1Hash - protocol of hashing
+// sha1Hash - sha1 hashing func
 func sha1Hash(data string) string {
 	hash := sha1.New()
 	hash.Write([]byte(data))
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
-// hashJson - do hashing
-func hashJson(data map[string]interface{}, whatHash []string) {
-	for k, v := range data {
-		strV, ok := v.(string)
-		if ok && exist(whatHash, k) {
-			data[k] = sha1Hash(strV)
-		}
-		if nk, ok := v.(map[string]interface{}); ok {
-			hashJson(nk, whatHash)
-		}
-	}
+// md5Hash - md5 hashing func
+func md5Hash(data string) string {
+	hash := md5.New()
+	hash.Write([]byte(data))
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
-// exist - check existing item in json file
-func exist(whatHash []string, k string) bool {
-	for _, v := range whatHash {
-		if v == k {
-			return true
+// hashJson - hash data to sh1 or md5
+func hashJson(data map[string]interface{}, whatHash []string, hashType *string) (string, error) {
+	for k, v := range data {
+		log.SetPrefix("INFO: ")
+		strV, ok := v.(string)
+
+		for _, hashKey := range whatHash {
+			if ok && k == hashKey {
+				if *hashType == "sha1" {
+					data[k] = sha1Hash(strV)
+					log.Printf("Hashed sha1: %v", data[k])
+				} else if *hashType == "md5" {
+					data[k] = md5Hash(strV)
+					log.Printf("Hashed md5: %v", data[k])
+				} else {
+					return "", fmt.Errorf("unsupported hash type: %v", *hashType)
+				}
+				break
+			}
+		}
+		if nestedMap, ok := v.(map[string]interface{}); ok {
+			_, err := hashJson(nestedMap, whatHash, hashType)
+			if err != nil {
+				return "", err
+			}
 		}
 	}
-	return false
+	resultJson, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(resultJson), nil
 }
