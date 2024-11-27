7265e40780327aca3f7228709d2c1663b3f6ab68
JuDyas <gden886@gmail.com>
2024-11-27 03:48:49 +0100

    Parsing Web-Site 1.0.4.main-update

--- Http_Learn/Parsing/ParsingWebSiteNew/main.go
+++ Http_Learn/Parsing/ParsingWebSiteNew/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	"GolangPractice/Http_Learn/Parsing/ParsingWebSiteNew/config"
+	"GolangPractice/Http_Learn/Parsing/ParsingWebSiteNew/database"
+	"GolangPractice/Http_Learn/Parsing/ParsingWebSiteNew/handlers"
 	"GolangPractice/Http_Learn/Parsing/ParsingWebSiteNew/tasks"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	var (
-		rdb = config.SetupRedis()
-		mux = http.NewServeMux()
+		rdb  = database.SetupRedis()
+		mux  = http.NewServeMux()
+		port = flag.String("port", ":8080", "http port")
 	)
-	SetupRoutes(mux, rdb)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	fs := http.FileServer(http.Dir("./html"))
-	mux.Handle("/html/", http.StripPrefix("/html", fs))
-	tasks.InitCron()
+	key := os.Getenv("VYZHENER_KEY")
+	if key == "" {
+		log.Fatal("VYZHENER_KEY env variable not set")
+	}
 
-	err := http.ListenAndServe(":8080", mux)
+	productChannel := make(chan []handlers.Product)
+	SetupRoutes(mux, rdb, productChannel)
+	go tasks.InitCron(productChannel, key, rdb)
+
+	err := http.ListenAndServe(*port, mux)
 	if err != nil {
 		log.Fatalf("start server error: %s", err)
 	}
+
+	// Struct for handlers with object for fork with bd and cipher
 }
